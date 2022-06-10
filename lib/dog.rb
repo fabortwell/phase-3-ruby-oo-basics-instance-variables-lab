c5beacf67b890a8f2fb0a15c0910579f6fd9d42e
Brian Sitati <briansit93@gmail.com>
2022-06-10 13:34:36 +0300

    commit tree 096eda622e73bcca56b81f755a2bb7254785dee9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,48 @@
 class Dog
-
+def name=(dogs_name)
+    @this_dogs_name = dogs_name
 end
+
+ def name
+    @this_dogs_name
+ end
+end
+
+lassie = Dog.new
+lassie.name ="Lassie"
+ puts lassie.name
+
+
+#  Defining setter and getter methods
+
+# class Dog
+
+#     #setter method
+#     def  name=(value)
+#         #assign the @name instance variable a value of whatever is passed as an argument
+#         @name = value
+#     end
+# end
+#  odie = Dog.new
+
+#  odie.name = "Odie"
+
+#  class Dog
+#     # setter method
+#     def name=(value)
+#       @name = value
+#     end
+  
+#     # getter method
+#     def name
+#       # access the data from the @name instance variable and return it
+#       @name
+#     end
+#   end
+  
+#   odie = Dog.new
+#   odie.name = "Odie"
+  
+#   # call the Dog#name method
+#   puts odie.name
+#   # "Odie"
